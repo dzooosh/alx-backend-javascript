626636cdf318687ab4dd698b43a410febba7e497
Alien <alongejoshua@gmail.com>
2023-01-25 17:50:48 +0100

    ES6 basics

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
   
     if (trueOrFalse) {
-      const task = true;
+       const task = true;
       const task2 = false;
     }
   
