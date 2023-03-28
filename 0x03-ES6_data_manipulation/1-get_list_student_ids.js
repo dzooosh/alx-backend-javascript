c18489ff83101579ca94008b5230a2bee7689a81
Alien <alongejoshua@gmail.com>
2023-03-28 18:23:04 +0100

    returns an array of ids from a list of object

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,10 @@
-export default function getListStudentIds(params) {
-  if (Array.isArray(params)) {
-    return params;
+export default function getListStudentIds(value) {
+  const result = [];
+  if (!Array.isArray(value)) {
+    return result;
   }
-  return [];
+  value.forEach((val) => {
+    result.push(val.id);
+  });
+  return result;
 }
