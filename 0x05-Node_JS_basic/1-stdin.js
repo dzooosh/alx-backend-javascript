4277f82ddddaeb91e70d63d699f1d5ba84bafeeb
Alien <alongejoshua@gmail.com>
2023-05-16 21:52:56 +0100

    followed eslint codestyle

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,19 @@
-process.stdout.write("Welcome to Holberton School, what is your name? \n");
+process.stdout.write('Welcome to Holberton School, what is your name? \n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     const input = data.toString().trim();
     console.log(`Your name is: ${input}`);
-    process.exit()
-    });
+    process.exit();
+  });
 } else {
   process.stdin.on('data', (data) => {
     const input = data.toString().trim();
     console.log(`Your name is: ${input}`);
-    });
+    process.exit();
+  });
   // when program wants to exit
   process.on('exit', () => {
-    console.log("This important software is now closing");
+    console.log('This important software is now closing');
   });
 }
