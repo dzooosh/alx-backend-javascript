bb85f1c1c6db23d31d6d43fa63afb581adaf7ff9
Alien <alongejoshua@gmail.com>
2023-05-16 16:02:43 +0100

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+const process = require('process')
+
+// input your name
+process.stdout.write("Welcome to Holberton School, what is your name? \n");
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    console.log(`Your name is: ${input}`);
+    process.exit()
+    });
+}
+else {
+  process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    console.log(`Your name is: ${input}`);
+    });
+  // when program wants to exit
+  process.on('exit', () => {
+    console.log("This important software is now closing");
+  });
+}
