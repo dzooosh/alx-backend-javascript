49b7f968f97eaa704fc0d876242b8e1af37d73a2
Alien <alongejoshua@gmail.com>
2023-05-18 16:33:39 +0100

    adds more to test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,30 +1,30 @@
-const assert = require('assert');
-const calculateNumber = require("./0-calcul.js");
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', function() {
-  it('should return the sum of the variables', function() {
-    const result = calculateNumber(1,3);
-    assert.equal(result, 4);
+describe("calculateNumber()", () => {
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
     });
 });
-
-describe('calculateNumber', function() {
-    it('should return the sum of the variables', function() {
-      const result = calculateNumber(1, 3.7);
-      assert.equal(result, 5);
-      });
-  });
-
-  describe('calculateNumber', function() {
-    it('should return the 5', function() {
-      const result = calculateNumber(1.2, 3.7);
-      assert.equal(result, 5);
-      });
-  });
-
-  describe('calculateNumber', function() {
-    it('should return 6', function() {
-      const result = calculateNumber(1.5, 3.7);
-      assert.equal(result, 6);
-      });
-  });
