97927df9879f6b02c9415a18da31bbe8df098ab0
Alien <alongejoshua@gmail.com>
2023-05-18 16:28:28 +0100

    update code

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,3 +7,24 @@ describe('calculateNumber', function() {
     assert.equal(result, 4);
     });
 });
+
+describe('calculateNumber', function() {
+    it('should return the sum of the variables', function() {
+      const result = calculateNumber(1, 3);
+      assert.equal(result, 4);
+      });
+  });
+
+  describe('calculateNumber', function() {
+    it('should return the sum of the variables', function() {
+      const result = calculateNumber(1.2, 3.7);
+      assert.equal(result, 5);
+      });
+  });
+
+  describe('calculateNumber', function() {
+    it('should return the sum of the variables', function() {
+      const result = calculateNumber(1.5, 3.7);
+      assert.equal(result, 6);
+      });
+  });
