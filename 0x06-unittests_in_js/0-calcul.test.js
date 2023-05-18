470ecead4d2e2e8349f48161727d49affad7615b
Alien <alongejoshua@gmail.com>
2023-05-18 16:31:14 +0100

    adds more to test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,20 +10,20 @@ describe('calculateNumber', function() {
 
 describe('calculateNumber', function() {
     it('should return the sum of the variables', function() {
-      const result = calculateNumber(1, 3);
-      assert.equal(result, 4);
+      const result = calculateNumber(1, 3.7);
+      assert.equal(result, 5);
       });
   });
 
   describe('calculateNumber', function() {
-    it('should return the sum of the variables', function() {
+    it('should return the 5', function() {
       const result = calculateNumber(1.2, 3.7);
       assert.equal(result, 5);
       });
   });
 
   describe('calculateNumber', function() {
-    it('should return the sum of the variables', function() {
+    it('should return 6', function() {
       const result = calculateNumber(1.5, 3.7);
       assert.equal(result, 6);
       });
